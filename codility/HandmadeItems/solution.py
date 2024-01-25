74c77148b8f499db1c9770eea7a50a00bc5342aa
Doug Slater <doug@slater.dev>
2024-01-25 07:23:57 -0500

    Improve HandmadeItems solution from O(n*n) to O(n)

--- codility/HandmadeItems/solution.py
+++ codility/HandmadeItems/solution.py
@@ -2,7 +2,7 @@
 # Then we can subtract from the input array each hour at the client's index i
 # and add that time to all waiting clients
 # The sum of the wait times is the answer
-def solution(T):
+def solution_slow(T):
     n = len(T)
     wait_times = [0] * n
 
@@ -35,8 +35,52 @@ def solution(T):
     ans = sum(wait_times)
     return int(ans % 10e9)
 
+# the above solution is O(N^2) because of the sum, inner while, and inner for.
+# goals: remove the sum, inner while, and inner for
+#   we can replace the sum with an initial sum of waiting clients and decrement
+#   then the outer while can be replaced with a for loop
+#   then we can replace the inner while with a continue 
+#   the inner for can be replaced with a counter of all wait times. for that we keep track of how many clients are done
+# Result: O(N)
+def solution_fast(T):
+    n = len(T)
+    c = -1 # currently serviced client
+    c_waiting = n # how many clients are still waiting 
+    total_wait = 0
+    work_done = [0] * n # hours worked for each client
+
+    # Loop until all work is done
+    while c_waiting > 0:
+
+        # Uncomment for debugging
+        #print(c, c_waiting, total_wait, work_done, T)
+
+        # Service next client 
+        c += 1
+
+        # Wrap around when all clients have been served a turn
+        c = c % n
+
+        # Skip finished clients
+        if client_done(c, work_done, T):
+            continue
+
+        # Increment all waiting clients' wait time
+        total_wait += c_waiting
+
+        # Service this client
+        work_done[c] += 1
+
+        if client_done(c, work_done, T):
+            c_waiting -= 1
+
+    return total_wait % 10e9
+
+def client_done(c, work_done, T):
+    return work_done[c] == T[c]
+
 def test(arr, want):
-  got = solution(list(arr))
+  got = solution_fast(list(arr))
   ok = want == got
   msg = "ok" if ok else f"bad, wanted {want}"
 
